538466298caada24e29aa217dd66bfe95e424415
JamieVaughn <wxv6@case.edu>
2022-02-09 02:14:06 -0500

    manually add log prefix to imgs

--- components/avatar.js
+++ components/avatar.js
@@ -1,7 +1,7 @@
 export default function Avatar({ name = 'Jamie Vaughn', picture = 'authors/jj.jpeg' }) {
   return (
     <div className="flex items-center">
-      <img src={picture} className="w-12 h-12 rounded-full mr-4" alt={name} />
+      <img src={'/log'+picture} className="w-12 h-12 rounded-full mr-4" alt={name} />
       <div className="text-xl font-bold">{name}</div>
     </div>
   )
--- components/cover-image.js
+++ components/cover-image.js
@@ -6,7 +6,7 @@ export default function CoverImage({ title, src, slug, height, width }) {
   const image = (
     <img
       // loader={({src, width, quality}) => `${src}`}
-      src={src}
+      src={'/log'+src}
       alt={`Cover Image for ${title}`}
       className={cn('responsive shadow-sm', {
         'hover:shadow-md transition-shadow duration-200': slug,
